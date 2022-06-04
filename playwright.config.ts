73bb9dfaac3a8ac562f152233095403e690c0e80
kobakazu0429 <kobakazu10@gmail.com>
2022-06-04 20:37:23 +0900

    add repoter

---
+++ playwright.config.ts
@@ -0,0 +1,8 @@
+import { type PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  outputDir: "vrt-test/out",
+  reporter: [["html", { open: "never", outputFolder: "vrt-test/report" }]],
+};
+
+export default config;
